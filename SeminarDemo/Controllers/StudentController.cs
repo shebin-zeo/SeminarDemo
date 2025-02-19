e082a9f33baaee65ce61b58097b8d2ddde554d31
shebin-zeo <shebin2024@gmail.com>
2025-02-19 20:56:14 +0530

    Add project files.

---
+++ SeminarDemo/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeminarDemo.Models;
+using System.Collections.Generic;
+
+namespace SeminarDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: /Student/Index
+        public IActionResult Index()
+        {
+            // Create a sample list of students
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Sumith", Email = "Sumith@example.com" },
+                new Student { Id = 2, Name = "Janaki", Email = "Janaki@example.com" },
+                 new Student { Id = 3, Name = "Safar", Email = "Safar@example.com" }
+            };
+
+            // Ensure we're passing a non-null list
+            return View(students);
+        }
+    }
+}
---
+++ SeminarDemo/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SeminarDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
